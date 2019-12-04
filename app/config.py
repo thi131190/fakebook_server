856341a2630cf0fd86d6d4e64e67f2f3233042cd
thi131190 <thi131190@gmail.com>
2019-12-04 15:45:01 +0700

    Init

---
+++ app/config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class Config(object):
+    SECRET_KEY = os.environ.get("FLASK_SECRET_KEY") or "supersekrit"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    GOOGLE_OAUTH_CLIENT_ID = os.environ.get("GOOGLE_OAUTH_CLIENT_ID")
+    GOOGLE_OAUTH_CLIENT_SECRET = os.environ.get("GOOGLE_OAUTH_CLIENT_SECRET")
